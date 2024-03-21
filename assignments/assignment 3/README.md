3e23f976e882109ce2c3b197c971ff872029f266
idajohannesen <idabj@hotmail.dk>
2024-03-21 13:48:25 +0100

    updating assignments

--- assignments/assignment 3/README.md
+++ assignments/assignment 3/README.md
@@ -1,8 +1,11 @@
 Short description:
+This code calculates how many songs from a given artist include words related to an input word. It takes both the artist and input word as arguments which can be decided by the user when running the code.
 
 Data:
+The dataset is a corpus of lyrics from 57,650 English-language songs. It can be found as a csv file in the input folder. 
+The data can be found here: https://www.kaggle.com/datasets/joebeachcapital/57651-spotify-songs
 
 Reproducing:
-The code can be run through the command line by inputting "python assignment3.py -i [word input] -a [artist]". A setup file has been included which should be run first to create a virtual environment with the necessary requirements
+The code can be run through the command line by inputting "python assignment3.py -i [word input] -a [artist]". A setup file has been included which should be run first to create a virtual environment with the necessary requirements. The artist must be part of the dataset.
 
 Discussion/summary:
--- assignments/assignment 3/src/assignment3.py
+++ assignments/assignment 3/src/assignment3.py
@@ -3,7 +3,7 @@ import argparse
 import gensim.downloader as api
 import pandas as pd
 
-def parser():
+def parser(): # create arguments for the artist and input word
     parser = argparse.ArgumentParser(description="Choosing an artist and a word")
     parser.add_argument("--artist",
                         "-a",
@@ -16,26 +16,26 @@ def parser():
     args = parser.parse_args()
     return args
 
-def load_data():
+def load_data(): # load the spotify song dataset
     filename = os.path.join("../input/Spotify_Million_Song_Dataset_exported.csv")
     data = pd.read_csv(filename)
     return data
 
-def download_model():
+def download_model(): # download a pretrained model
     model = api.load("glove-wiki-gigaword-50")
     return model
 
-def find_similar_words(model, args):
+def find_similar_words(model, args): # finding top 10 most similar words to the input word
     most_similar=model.most_similar(args.input, topn=10)
-    words, similarity = zip(*most_similar)
+    words, similarity = zip(*most_similar) # split the output into a word variable and a similarity variable
     return words
     print(words)
 
-def select_artist(data, args):
+def select_artist(data, args): # create a list of the chosen artist's lyrics
     search_docs = list(data[data["artist"]==args.artist]["text"])
     return search_docs
 
-def counter(search_docs, words):
+def counter(search_docs, words): # count how many times the words appear in the lyrics
     doc_counter = 0
     for doc in search_docs:
         if any(word in doc.split() for word in words):
@@ -43,8 +43,8 @@ def counter(search_docs, words):
     return doc_counter
 
 def print_results(doc_counter, search_docs, args):
-    percent = round(doc_counter/len(search_docs)*100, 1)
-    print(percent, "% of", args.artist, "'s songs contain words related to", args.input)
+    percent = round(doc_counter/len(search_docs)*100, 1) # calculate percentages and round to a single decimal point
+    print(percent, "% of", args.artist, "'s songs contain words related to", args.input) # print the results
 
 def main():
     args = parser()
