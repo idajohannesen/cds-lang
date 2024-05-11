19aa0782c5f273359fa71405465b9b76fcb38eb4
idajohannesen <idabj@hotmail.dk>
2024-05-11 14:40:39 +0200

    updating assignments

--- assignments/assignment 4/README.md
+++ assignments/assignment 4/README.md
@@ -1,12 +1,23 @@
-Short description: This code goes through the script of all 8 seasons of Game of Thrones and assigns each line an emotion label: anger, disgust, fear, joy, neutral, sadness, or surprise. Two plots are created: the distribution of emotions for every season and the relative frequency of an emotion label across all 8 seasons.
+# Assignment 4
 
-Data:
-The dataset consists of scripts from all seasons of the TV show Game of Thrones, split into lines. It can be inserted as a .csv file in the input folder.
+# Short description: 
+This code goes through the script of all 8 seasons of Game of Thrones and assigns each line an emotion label: anger, disgust, fear, joy, neutral, sadness, or surprise. Two plots are created: the distribution of emotions for every season and the relative frequency of an emotion label across all 8 seasons.
+
+This code uses CodeCarbon to track CO₂ emissions. The results are discussed in Assignment 5.
+
+# Data:
+The dataset consists of scripts from all seasons of the TV show Game of Thrones, split into lines. It can be inserted as a .csv file in the ```input``` folder.
 The data can be downloaded from here: https://www.kaggle.com/datasets/albenft/game-of-thrones-script-all-seasons?select=Game_of_Thrones_Script.csv
 
-Reproducing:
-A setup file has been included which can be run to create a virtual environment with the necessary requirements. The script requires that a csv file of the data is located in the input folder. The code can be run through the command line by inputting "python assignment4.py" from the src folder.
+# Reproducing:
+A setup file has been included which creates a virtual environment with the necessary requirements, as well as a ```run.sh``` file which activates the virtual environment and runs the script. The script requires that the data is located in the ```input``` folder with the structure mentioned above. 
 
-Discussion/summary:
+Start by running the setup file by inputting ```bash setup.sh``` in the terminal. 
+The code can then be run through the terminal by inputting ```bash run.sh```.
+Both of these lines should be executed from the ```assignment 4``` folder.
+
+The code has been written and tested on a Windows laptop and may not work correctly on other operating systems.
+
+# Discussion/summary:
 The script succesfully creates plots over the distribution of emotion labels and seasons. The vast majority of lines are labeled as being 'neutral', with relative frequencies ranging from around 45% to 52%. By looking at the results in the .csv file, we can see that many of the shorter lines are considered neutral in tone. This makes sense, since many of them are simple replies, yes/no's or names being mentioned. These would not have obvious emotional associations based on the text, but a viewer would have access to the general tone of the line and be able to infer emotion from this. Therefore, the labeling does have its limitations since it is purely text-based and ends up with excessive neutral labels. 
 We can note that disgust is generally decreasing. Our plots also show that anger, surprise, and disgust are the most common emotions after the neutral label. Joy is often the least frequent label in a season, except for season 8 and 5 where fear is the least frequent emotion. Based on this, Game of Thrones comes off as a series full of conflict and only a few moments of joy per season. 
--- assignments/assignment 4/src/assignment4.py
+++ assignments/assignment 4/src/assignment4.py
@@ -1,20 +1,57 @@
+from codecarbon import EmissionsTracker
 import os
+# CodeCarbon tracker
+outfolder = os.path.join("..", "assignment 5", "input") # set output folder
+tracker = EmissionsTracker(project_name="assignment 4", # create tracker
+                           experiment_id="emotion analysis of GoT",
+                           output_dir=outfolder,
+                           output_file="emissions.csv")
+tracker.start_task("load packages")
+
+# creating dataframes
 import pandas as pd
+
+# loading model
 from transformers import pipeline
+
+# visualization
 import matplotlib.pyplot as plt
 
-def load_classifier(): # load classifier pipeline from HuggingFace
+tracker.stop_task()
+
+def load_classifier():
+    """
+    Loads classifier pipeline from HuggingFace
+    
+    Returns:
+        classifier: emotion classifier
+    """
     classifier = pipeline("text-classification", 
                           model="j-hartmann/emotion-english-distilroberta-base", 
                           return_all_scores=False) # return only the most likely score
     return classifier
 
-def load_data(): # navigating into the input folder and reading the dataset as a pandas dataframe
-    filename = os.path.join("../input/Game_of_Thrones_Script.csv")
-    data = pd.read_csv(filename, keep_default_na=False)
+def load_data():
+    """
+    Loads the data
+    
+    Returns:
+        data: dataset as pandas dataframe
+    """
+    filename = os.path.join("input/Game_of_Thrones_Script.csv") # navigating into the input folder
+    data = pd.read_csv(filename, keep_default_na=False) # reading the dataset as a pandas dataframe
     return data
 
-def get_scores(data, classifier): # using a for loop to get an emotion label for every line in the dataset
+def get_scores(data, classifier):
+    """
+    Adds an emotion label to every line in the dataset
+
+    Arguments:
+        data: dataset as pandas dataframe
+        classifier: emotion classifier
+    Returns:
+        scores: list of emotion labels + likeliness scores
+    """
     i = 1
     scores = [] # make an empty list for every emotion label to go into
     for row in data["Sentence"]:
@@ -24,7 +61,15 @@ def get_scores(data, classifier): # using a for loop to get an emotion label for
         i += 1
     return scores
 
-def get_labels(scores): # removing the scores from the list 'scores' and leaving only the label names
+def get_labels(scores):
+    """
+    Removes the scores from the list 'scores' and leaving only the label names
+    
+    Arguments:
+        scores: list of emotion labels + likeliness scores
+    Returns:
+        emotions: list of emotion labels
+    """
     row = 0 # create a row counter to use for the slice
     emotions = [] # create list for emotion labels to go in
 
@@ -34,26 +79,37 @@ def get_labels(scores): # removing the scores from the list 'scores' and leaving
         row += 1 # go to next row
     return emotions
 
-def update_dataframe(emotions, data): # add the emotion labels as an extra column in the dataframe
+def update_dataframe(emotions, data):
+    """
+    Adds the emotion labels as an extra column in the dataframe
+    
+    Arguments:
+        emotions: list of emotion labels
+        data: dataset as pandas dataframe
+    Returns:
+        new_df: updated dataframe with emotion labels
+    """
     # Using DataFrame.insert() to add a column
     new_df = data # clone the dataframe before adding to it
     new_df.insert(6, #insert as the 6th column
                 "Emotion_label", #column name
                 emotions, # data to insert
                 True) # allow duplicates
-    new_df.to_csv("../output/dataframe.csv", index=False) # upload dataframe to output folder
+    new_df.to_csv("output/dataframe.csv", index=False) # upload dataframe to output folder
     return new_df
 
-def emotion_distribution_plot(new_df): # create a plot for the distribution of emotion labels for every season
-    counter = 1 # create a counter to update the season labels
+def emotion_distribution_plot(new_df):
+    """
+    Creates a plot for the distribution of emotion labels for every season
+    
+    Arguments:
+        new_df: updated dataframe with emotion labels
+    """
     season_list = ["Season 1", "Season 2", "Season 3", "Season 4", "Season 5", "Season 6", "Season 7", "Season 8"]
-    season_length = []
     for season in season_list:
         season_label = season # create a label for every season
         season_df = new_df[new_df['Season'] == season_label] #split dataframe based on seasons
         sorted_df = season_df.sort_values('Emotion_label', inplace=False) # sort the dataframe so the plots appear consistently
-        length = len(sorted_df)
-        season_length.append(length) # save the total amount of lines for each season, is used later to plot relative frequencies
         
         plt.figure(figsize = (16,6))
         plt.hist(sorted_df["Emotion_label"], bins = 7) # plot histogram
@@ -61,28 +117,25 @@ def emotion_distribution_plot(new_df): # create a plot for the distribution of e
         plt.xlabel('Emotion labels')
         plt.ylabel('Frequencies')
 
-        plt.savefig('../output/' + season_label + '.png') # save output
+        plt.savefig('output/' + season_label + '.png') # save output
         plt.show()
         plt.clf()
-        
-        counter += 1
-        return season_list, season_length
     
-def season_distribution_plot(new_df, season_length): # create a plot for the relative frequency of every emotion label across seasons    
+def season_distribution_plot(new_df):
+    """
+    Creates a plot for the relative frequency of every emotion label across seasons
+
+    Arguments:
+        new_df: updated dataframe with emotion labels
+    """  
     emotion_list = ["anger", "disgust", "fear", "joy", "neutral", "sadness", "surprise"]
-    emotion_counter = 0
     season_list = ["Season 1", "Season 2", "Season 3", "Season 4", "Season 5", "Season 6", "Season 7", "Season 8"]
-    season_counter = 1
     for emotions in emotion_list:
         rel_freq_list = [] # save relative frequencies per emotion
-        length_counter = 0 # create counter to loop through the list of every season's length
         for seasons in season_list:
             emotion_df = new_df[(new_df["Emotion_label"] == emotions)&(new_df["Season"] == seasons)] #split dataframe based on seasons
-            #emotion_amt = float(len(emotion_df))
-            #total_amt = float(season_length[length_counter])
             rel_freq = len(emotion_df)/len(new_df[new_df["Season"]==seasons])*100 # calculate relative frequency
             rel_freq_list.append(rel_freq)
-            length_counter += 1
         
         plt.figure(figsize = (16,6))
         plt.plot(season_list, rel_freq_list) # plot
@@ -90,21 +143,40 @@ def season_distribution_plot(new_df, season_length): # create a plot for the rel
         plt.xlabel('Seasons')
         plt.ylabel('Relative frequencies')
 
-        plt.savefig('../output/' + emotions + '.png') # save output
+        plt.savefig('output/' + emotions + '.png') # save output
         plt.show()
         plt.clf()
-        
-        emotion_counter += 1
-        season_counter += 1
 
 def main():
+    tracker.start_task("load model")
     classifier = load_classifier()
+    tracker.stop_task()
+
+    tracker.start_task("load data")
     data = load_data()
+    tracker.stop_task()
+
+    tracker.start_task("sentiment analysis")
     scores = get_scores(data, classifier)
+    tracker.stop_task()
+
+    tracker.start_task("fix labels")
     emotions = get_labels(scores)
+    tracker.stop_task()
+
+    tracker.start_task("update dataframe")
     new_df = update_dataframe(emotions, data)
-    season_length = emotion_distribution_plot(new_df)
-    season_distribution_plot(new_df, season_length)
+    tracker.stop_task()
+
+    tracker.start_task("plot distribution of emotion labels for every season")
+    emotion_distribution_plot(new_df)
+    tracker.stop_task()
+
+    tracker.start_task("plot emotion freq across seasons")
+    season_distribution_plot(new_df)
+    tracker.stop_task()
+    
+    tracker.stop()
 
 if __name__=="__main__":
     main()
